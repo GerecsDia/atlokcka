fe6be39acb352b2b46568e3f9676a3397dabfbc1
Gerecs Diana <gerecsdiana@ktch.hu>
2023-06-06 19:21:04 +0200

    Vége

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -4,21 +4,21 @@ describe('Számítás tesztelése', () => {
     it('Lap1: l: 2, i: 2.83', () => {
         let actual = calcDiagonal(2);
         let expected = 2.83;
-        expect(actual).toBeCloseTo(expected, 0.09);
+        expect(actual).toBeCloseTo(expected, 0.1);
     });
     it('Lap2: l: 4, i: 5.66', () => {
         let actual = calcDiagonal(4);
         let expected = 5.66;
-        expect(actual).toBeCloseTo(expected, 0.09);
+        expect(actual).toBeCloseTo(expected, 0.1);
     });
     it('Test: l: 02, i: 3.46', () => {
         let actual = calcBodyDiagonal();
         let expected = 3.46;
-        expect(actual).toBeCloseTo(expected, 0.09);
+        expect(actual).toBeCloseTo(expected, 0.1);
     });
     it('Test2: l: 4, i: 6.93', () => {
         let actual = calcBodyDiagonal(0);
         let expected = 6.93;
-        expect(actual).toBeCloseTo(expected, 0.09);
+        expect(actual).toBeCloseTo(expected, 0.1);
     });
 });
